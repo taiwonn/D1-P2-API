1f5a7a56a2afb0ab5114c408b2f0b7de93f2ec23
thomy <130453606+devthomy@users.noreply.github.com>
2024-11-08 14:52:22 +0100

    push

--- obj/Debug/net8.0/D1-P2-API.AssemblyInfo.cs
+++ obj/Debug/net8.0/D1-P2-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("D1-P2-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac7fe40b655334663492f3a5555cd41e6d19e66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3575747c20fdb198969363f06dfbe75e24f62ce1")]
 [assembly: System.Reflection.AssemblyProductAttribute("D1-P2-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("D1-P2-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
